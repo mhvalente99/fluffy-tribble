bf05b8246684c8b2a81d8d026d04ee45536f180d
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-22 15:15:04 -0300

    feat: implement dependency inject to service CreateSpecification

--- src/routes/spefications.routes.ts
+++ src/routes/spefications.routes.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
+import { container } from "tsyringe";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const createSpecificationService = new CreateSpecificationService(
-        specificationsRepository
+    const createSpecificationService = container.resolve(
+        CreateSpecificationService
     );
 
     createSpecificationService.execute({ name, description });
