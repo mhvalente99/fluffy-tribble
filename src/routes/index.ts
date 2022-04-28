3ab2d0bfb2cb9fe3be7c403df2d58fb47815c1d9
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-28 08:58:00 -0300

    chore: rename file specifications

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
-import { specificationsRoutes } from "./spefications.routes";
+import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
