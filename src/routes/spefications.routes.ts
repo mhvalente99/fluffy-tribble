86f3d6e4e6c77e5e9679ba3763a463eddccb73a7
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-28 08:56:51 -0300

    feat: use middleware in specifications routes

--- src/routes/spefications.routes.ts
+++ src/routes/spefications.routes.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import { container } from "tsyringe";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
+specificationsRoutes.use(ensureAuthenticated);
+specificationsRoutes.post("/", async (request, response) => {
     const { name, description } = request.body;
 
     const createSpecificationService = container.resolve(
         CreateSpecificationService
     );
 
-    createSpecificationService.execute({ name, description });
+    await createSpecificationService.execute({ name, description });
 
     return response.status(201).send();
 });
