a809f0aabf547cb128d92364a90e1d1c2c5b44ca
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-05-18 08:45:24 -0300

    test: create test to not create a car with exists license plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
@@ -21,5 +22,29 @@ describe("Create Car", () => {
             brand: "Brand Car",
             category_id: "category",
         });
+    });
+
+    it("should not be able to create a car with exists license plate", () => {
+        expect(async () => {
+            await createCarUseCase.execute({
+                name: "Name Car 1",
+                description: "Description Car 1",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand Car 1",
+                category_id: "category 1",
+            });
+
+            await createCarUseCase.execute({
+                name: "Name Car 2",
+                description: "Description Car 2",
+                daily_rate: 120,
+                license_plate: "ABC-1234",
+                fine_amount: 80,
+                brand: "Brand Car 2",
+                category_id: "category 2",
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
