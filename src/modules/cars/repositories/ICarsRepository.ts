4e7f773267825f731991f852c716ea39bc83e596
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-05-18 08:43:30 -0300

    feat: tdd - add method to find a car by license plate

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -3,6 +3,7 @@ import { Car } from "../infra/typeorm/entities/Car";
 
 interface ICarsRepository {
     create(data: ICreateCarDTO): Promise<void>;
+    findByLicensePlate(license_plate: string): Promise<Car>;
 }
 
 export { ICarsRepository };
