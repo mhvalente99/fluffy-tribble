8f69aab05d13bcd523732a3b742ba16271550f64
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-02 19:20:06 -0300

    test: e2e - create a new category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,52 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "@shared/infra/database/typeorm";
+import { app } from "@shared/infra/http/app";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+
+        await connection.runMigrations();
+
+        const id = uuidV4();
+        const password = await hash("admin", 8);
+
+        await connection.query(
+            `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+            VALUES('${id}', 'admin', 'admin@rentalx.com.br', '${password}', true, 'now()', '')
+            `
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("should be able to create a new category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentalx.com.br",
+            password: "admin",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Test",
+                description: "Category Test",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+    });
+});
