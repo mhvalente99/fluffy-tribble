530ff3adb322ee3d27e725e5a5b9970ae6cc2f9b
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-07-13 10:16:00 -0300

    config: create file to configure information about tokens

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    secret_token: "4bc34223c51b2fc67010b3b9b1048c13",
+    expires_in_token: "15m",
+    secret_refresh_token: "502153ce20fc0eed0e6ef5f40ef73b93",
+    expires_in_refresh_token: "30d",
+};
