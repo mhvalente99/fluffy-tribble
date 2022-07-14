0b1a7250853096abd0eba1d297b2785e7f539eb9
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-07-13 22:14:15 -0300

    feat: verify user authenticated by refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -24,12 +25,15 @@ export async function ensureAuthenticated(
     try {
         const { sub: user_id } = verify(
             token,
-            "4bc34223c51b2fc67010b3b9b1048c13"
+            auth.secret_refresh_token
         ) as IPayload;
 
-        const usersRepository = new UsersRepository();
+        const usersTokensRepository = new UsersTokensRepository();
 
-        const user = await usersRepository.findById(user_id);
+        const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
 
         if (!user) {
             throw new AppError("User does not exists!", 401);
