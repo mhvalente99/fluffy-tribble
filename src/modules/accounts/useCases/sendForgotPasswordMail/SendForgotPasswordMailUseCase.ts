8c9960e7adfd095d1e6dcbce7ddd35080351759f
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-07-18 08:46:13 -0300

    refactor: fix new parameters and configure variables in method execute

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
@@ -27,6 +28,15 @@ class SendForgotPasswordMailUseCase {
             throw new AppError("User does not exists");
         }
 
+        const templatePath = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "emails",
+            "forgotPassword.hbs"
+        );
+
         const token = uuidV4();
 
         const expires_date = this.dateProvider.addHoursInDate(
@@ -40,10 +50,16 @@ class SendForgotPasswordMailUseCase {
             expires_date,
         });
 
+        const variables = {
+            name: user.name,
+            link: `${process.env.FORGOT_MAIL_URL}${token}`,
+        };
+
         await this.mailProvider.sendMail(
             email,
             "Recuperação de Senha",
-            `O link para o rest é ${token}`
+            variables,
+            templatePath
         );
     }
 }
